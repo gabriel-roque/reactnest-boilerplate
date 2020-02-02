c9813fc3c80ff2e56dc8621c1494e6d300d1fd00
gabriel-roque <gabrielrms.dll@gmail.com>
2020-02-02 00:29:22 -0300

    feat: logo reactdones in home project

--- web-app/src/modules/home/pages/Home/Home.jsx
+++ web-app/src/modules/home/pages/Home/Home.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  container: {
+    height: "98vh"
+  }
+}));
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          className={classes.container}
+          xs={12}
+        >
+          <img src={require("assets/img/reactdonis.jpg")} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
