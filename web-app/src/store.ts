ac23bdd2ada2f80002b27d57aedbc64bb59d0814
gabriel-roque <gabrielrms.dll@gmail.com>
2020-05-15 14:55:24 -0300

    refactor(web-app): validate redux devtools enable in development

--- web-app/src/store.ts
+++ web-app/src/store.ts
@@ -9,13 +9,15 @@ const reducers = combineReducers({ auth });
 
 declare global {
   interface Window {
-    // eslint-disable-next-line no-undef
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
 
-// eslint-disable-next-line no-undef
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let composeEnhancer = compose;
+
+if (process.env.NODE_ENV === 'development') {
+  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
 
 const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
