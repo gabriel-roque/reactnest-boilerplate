d7249fbbb8c00826df3a44057ece5d0a2a4333e1
gabriel-roque <gabrielrms.dll@gmail.com>
2021-01-05 19:12:36 -0300

    refactor: reorganize structure for version 3

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,19 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'api',
-      script: 'server.js',
+      name: 'react-nest-v3',
+      script: 'dist/main.js',
 
       instances: 2,
+      exec_mode: 'cluster',
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
